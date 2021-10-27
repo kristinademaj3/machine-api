951d9e575cd9e26f2acd3dc3cf9ad90fe89ccbe5
kristinademaj3 <kristina.demaj3@gmail.com>
2021-10-27 23:15:31 +0200

    Changes on startup

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,6 @@ namespace machine_api
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
